3b901b6849f9c4e20454fed8ee88b7ed9a978249
Tor Arne Birkeland <torabir@stud.ntnu.no>
2024-10-11 11:45:15 +0200

    ...

---
+++ DCST-2002-Webutvikling/ov06-client-tests copy/todo-client-tests/client/src/task-service.tsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/api/v2';
+
+export type Task = {
+  id: number;
+  title: string;
+  done: boolean;
+  description: string
+};
+
+class TaskService {
+  /**
+   * Get task with given id.
+   */
+  get(id: number) {
+    return axios.get<Task>('/tasks/' + id).then((response) => response.data);
+  }
+
+  /**
+   * Get all tasks.
+   */
+  getAll() {
+    return axios.get<Task[]>('/tasks').then((response) => response.data);
+  }
+
+  /**
+   * Create new task having the given title.
+   *
+   * Resolves the newly created task id.
+   */
+  create(title: string, description: string) {
+    return axios
+      .post<{ id: number }>('/tasks', { title: title , description: description})
+      .then((response) => response.data.id);
+  }
+
+    /**
+   * Update a task with the given id.
+   */
+    update(task: Task) {
+      return axios
+        .put('/tasks/' + task.id, task)  // Bruker PUT for å oppdatere oppgaven
+        .then((response) => response.data);
+    }
+      /**
+   * Delete task with given id.
+   */
+  delete(id: number) {
+    return axios.delete('/tasks/' + id);
+  }
+}
+
+
+const taskService = new TaskService();
+export default taskService;
---
+++ DCST-2002-Webutvikling/ov06-client-tests copy/todo-client-tests/server/src/task-service.ts
@@ -0,0 +1,85 @@
+import pool from './mysql-pool';
+import type { RowDataPacket, ResultSetHeader } from 'mysql2';
+
+export type Task = {
+  id: number;
+  title: string;
+  done: boolean;
+  description: string
+};
+
+class TaskService {
+  /**
+   * Get task with given id.
+   */
+  get(id: number) {
+    return new Promise<Task | undefined>((resolve, reject) => {
+      pool.query('SELECT * FROM Tasks WHERE id = ?', [id], (error, results: RowDataPacket[]) => {
+        if (error) return reject(error);
+
+        resolve(results[0] as Task);
+      });
+    });
+  }
+
+  /**
+   * Get all tasks.
+   */
+  getAll() {
+    return new Promise<Task[]>((resolve, reject) => {
+      pool.query('SELECT * FROM Tasks', (error, results: RowDataPacket[]) => {
+        if (error) return reject(error);
+
+        resolve(results as Task[]);
+      });
+    });
+  }
+
+  /**
+   * Create new task having the given title.
+   *
+   * Resolves the newly created task id.
+   */
+  create(title: string, description: string) { // må inklludere description ? 
+    return new Promise<number>((resolve, reject) => {
+      pool.query('INSERT INTO Tasks SET title=?, description=?', [title, description], (error, results: ResultSetHeader) => {
+        if (error) return reject(error);
+
+        resolve(results.insertId);
+      });
+    });
+  }
+
+  /**
+   * Delete task with given id.
+   */
+  delete(id: number) {
+    return new Promise<void>((resolve, reject) => {
+      pool.query('DELETE FROM Tasks WHERE id = ?', [id], (error, results: ResultSetHeader) => {
+        if (error) return reject(error);
+        if (results.affectedRows == 0) return reject(new Error('No row deleted'));
+
+        resolve();
+      });
+    });
+  }
+  
+    /**
+   * Update task with given id.
+   */
+    update(id: number, title: string, description: string, done: boolean) {
+      return new Promise<void>((resolve, reject) => {
+        pool.query(
+          'UPDATE Tasks SET title=?, description=?, done=? WHERE id=?',
+          [title, description, done, id],
+          (error, _results) => {
+            if (error) return reject(error);
+            resolve();
+          }
+        );
+      });
+    }  
+}
+
+const taskService = new TaskService();
+export default taskService;
